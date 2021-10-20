296c9b9600de24aeda809a18e4f121639ba71dac
Elithon Silva <elithonsilva@gmail.com>
2021-10-20 19:58:27 -0300

    correcoes no arquivo app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
-import Settings from './pages/Settings';
+import ButtonSettings from './pages/Settings';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/settings" component={ Settings } />
+        <Route path="/settings" component={ ButtonSettings } />
         <Route path="/game" component={ Game } />
         <Route exact path="/" component={ Login } />
       </Switch>
