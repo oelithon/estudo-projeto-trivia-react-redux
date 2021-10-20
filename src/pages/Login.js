9c7ae042557bdb65f9ed56947c964c53cc28f805
Mateus Gambaro <magambaro@hotmail.com>
2021-10-20 18:15:50 -0300

    Requisito 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ButtonSettings from '../components/loginComponents/ButtonSettings';
 import LoginFormComponent from '../components/loginComponents/LoginFormComponent';
 
 class Login extends React.Component {
   render() {
     return (
-      <LoginFormComponent />
+      <>
+        <LoginFormComponent />
+        <ButtonSettings />
+      </>
     );
   }
 }
---
+++ src/pages/Settings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <section>
+        <h2 data-testid="settings-title">Configurações</h2>
+      </section>
+    );
+  }
+}
+
+export default Settings;
